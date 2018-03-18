1fc76e1f018f16e69d4c93ff7d0073f77c52128a
Wil <wgadams82@gmail.com>
2018-03-18 18:25:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/wil/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.example.wil.geoquiz;
+
+/**
+ * Created by Wil on 3/18/2018.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+
+        mAnswerTrue = answerTrue;
+
+    }
+}
